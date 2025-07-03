416e2ebc5bc3ea5e6dfc3bb7832d826260061b72
Yurii Soltys <y.soltys@scalr.com>
2025-07-03 13:43:32 +0300

    Update variables.tf

--- modules/scalr/agent-pool/variables.tf
+++ modules/scalr/agent-pool/variables.tf
@@ -3,3 +3,14 @@ variable "agent_pool_name" {
   type        = string
   default     = "webhook-agent-pool"
 }
+
+variable "scalr_hostname" {
+  type = string
+  description = "host name of Scalr instance"
+  default = "mainiacp.soltys-test.testenv.scalr.dev"
+}
+
+variable "scalr_token" {
+  type = string
+  description = "Scalr token"
+}
