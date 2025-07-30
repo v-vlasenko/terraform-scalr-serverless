35d59179ee2d481bafdd4a64226b819211691b35
Vladyslav Vlasenko <v.vlasenko@scalr.com>
2025-07-30 14:08:04 +0300

    Update variables.tf

--- modules/scalr/agent-pool/variables.tf
+++ modules/scalr/agent-pool/variables.tf
@@ -13,5 +13,4 @@ variable "scalr_hostname" {
 variable "scalr_token_sub" {
   type = string
   description = "Scalr token"
-  default     = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NfaWQiOiJhY2MtdjBvdGk0dWtmMmU4NzZkOXIiLCJleHAiOjE3NjE2NDk1MjMsImlzcyI6InNjYWxyOnVzZXIiLCJqdGkiOiJhdC12MG90aTcwMm05cmJrYjNrYiJ9.vc-UVAFt8s_-mjlPi-RHcS-wxN7TfBWuleGuRYysB5E"
 }
